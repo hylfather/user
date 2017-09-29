9df091713d34a79bb43a48dd0f36a586800d5d62
hylfather <352142762@qq.com>
2017-09-29 13:50:16 +0800

    Add files via upload

---
+++ gdg.py
@@ -0,0 +1,10 @@
+a = "S!@#@1FD23154A34"
+li = list(a)
+
+for i in range(14,0,-2):
+    for j in range(12,0,-4):
+        li[j],li[j-4] = li[j-4],li[j]
+    li[i-1],li[i-2] = li[i-2],li[i-1]
+    
+b=''.join(li)
+print(b)
